b4198fa083dc99c1e13cb1ddb3e08c9a1346a943
benz0id <bentudorprice@gmail.com>
2022-07-27 11:18:17 -0400

    -

--- seq_alignment_analyser/scoring.py
+++ seq_alignment_analyser/scoring.py
@@ -1,5 +1,21 @@
 
 
 class ScoreBindingPair:
-    """Re"""
+    """Responsible for generating unified scores that capture the features of
+    binding pairs.
+
+    === Private Attributes ===
+
+    _melting_temps: The melting temperatures of all binding sequences included
+        so far.
+
+    _primer_parts: Stores all components necessary to construct primer sets.
+
+
+
+
+    """
+
+
+
 
