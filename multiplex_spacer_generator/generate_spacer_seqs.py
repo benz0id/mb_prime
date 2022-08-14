6499eeffd2cd74405ced6ca3a857374db14eec73
benz0id <bentudorprice@gmail.com>
2022-08-14 19:21:26 -0400

    minor test chages

--- multiplex_spacer_generator/generate_spacer_seqs.py
+++ multiplex_spacer_generator/generate_spacer_seqs.py
@@ -4,7 +4,7 @@ import math
 import logging
 from multiplex_spacer_generator.binding_align import SpacerCombo
 import random
-from multiprocessing import Queue, Manager, Process
+import multiprocessing as mp
 from time import time, sleep
 from statistics import mean
 from multiplex_spacer_generator.primer_pool import PrimerPool, get_all_dgs
@@ -25,6 +25,27 @@ NUM_ITER_BEFORE_CHECK = 100
 ALERT_EVERY = 1000
 
 MIN_DG = -10000000000000000000
+
+class Process(mp.Process):
+    def __init__(self, *args, **kwargs):
+        mp.Process.__init__(self, *args, **kwargs)
+        self._pconn, self._cconn = mp.Pipe()
+        self._exception = None
+
+    def run(self):
+        try:
+            mp.Process.run(self)
+            self._cconn.send(None)
+        except Exception as e:
+            tb = traceback.format_exc()
+            self._cconn.send((e, tb))
+            # raise e  # You can still rise this exception if you need to
+
+    @property
+    def exception(self):
+        if self._pconn.poll():
+            self._exception = self._pconn.recv()
+        return self._exception
 
 
 def gen_heterogeneity_alignment(seqs: List[str], spacers: SpacerCombo) \
@@ -104,7 +125,7 @@ def get_best_heterogeneity_spacer_seqs(
         f_5p: List[str], f_binding: List[str], r_5p: List[str],
         r_binding: List[str], f_spacers: SpacerCombo, r_spacers: SpacerCombo,
         allowed_seconds: int, num_structs_to_avg: int,
-        out_queue: Queue) -> None:
+        out_queue: mp.Queue) -> None:
     """Runs for <allowed_seconds> after which the best PrimerPool will be
     returned."""
     best_set = None
@@ -163,7 +184,7 @@ def get_best_heterogeneity_spacer_seqs_threadable(
         num_threads: int) -> PrimerPool:
     """Returns the primer pool least predisposed to forming very stable dimer
     structures."""
-    manager = Manager()
+    manager = mp.Manager()
     thread_out_queue = manager.Queue()
     fin_time = time() + allowed_seconds
 
